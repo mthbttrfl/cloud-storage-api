a15c589c924b16ab8df0d89f635739a7acc2053c
mthbttrfl <mthbttrfl@google.com>
2026-02-06 00:16:20 +0300

    fix: fix generic type

--- src/main/java/org/example/cloudstorageapi/repository/UserRepository.java
+++ src/main/java/org/example/cloudstorageapi/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package org.example.cloudstorageapi.repository;
 
+import org.example.cloudstorageapi.entity.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository<T, ID> extends JpaRepository<T, ID> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-    Optional<T> findByUsername(String username);
+    Optional<UserModel> findByUsername(String username);
 }
