ec9414ded3b04e96b1ce8408f445663b00ac4b6e
mthbttrfl <mthbttrfl@google.com>
2026-02-05 17:21:46 +0300

    feat: add Entity UserModel

---
+++ src/main/java/org/example/cloudstorageapi/entity/UserModel.java
@@ -0,0 +1,24 @@
+package org.example.cloudstorageapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
