a9a26e6b661f0445ae41bdab42e668127d162e93
mthbttrfl <mthbttrfl@google.com>
2026-02-06 00:16:53 +0300

    fix: add packages

--- src/main/java/org/example/cloudstorageapi/config/DataJpaConfig.java
+++ src/main/java/org/example/cloudstorageapi/config/DataJpaConfig.java
@@ -1,11 +1,13 @@
 package org.example.cloudstorageapi.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories
+@EntityScan(basePackages = "org.example.cloudstorageapi.entity")
+@EnableJpaRepositories(basePackages = "org.example.cloudstorageapi.repository")
 @EnableTransactionManagement
 public class DataJpaConfig {
 }
