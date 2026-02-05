200f448add61ed04b82b7b3270e38f6fe90a4a07
mthbttrfl <mthbttrfl@google.com>
2026-02-05 05:39:36 +0300

    init

---
+++ src/main/java/org/example/cloudstorageapi/CloudStorageApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloudstorageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloudstorageapi/CloudStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudstorageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
