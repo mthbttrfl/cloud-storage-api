c6d1c0748620834f30d1168a0585fafa354a5e3f
mthbttrfl <mthbttrfl@google.com>
2026-03-13 16:17:03 +0300

    perf: improved resource loading logic
    - useless sorting has been removed
    - added IOException check

--- src/main/java/org/example/cloudstorageapi/service/storage/minio/MinIOStorageUploadService.java
+++ src/main/java/org/example/cloudstorageapi/service/storage/minio/MinIOStorageUploadService.java
@@ -2,6 +2,7 @@ package org.example.cloudstorageapi.service.storage.minio;
 
 import lombok.RequiredArgsConstructor;
 import org.example.cloudstorageapi.dto.resp.RespResourceInfoDTO;
+import org.example.cloudstorageapi.exception.StorageUploadException;
 import org.example.cloudstorageapi.service.storage.StorageResourceService;
 import org.example.cloudstorageapi.service.storage.StorageUploadService;
 import org.example.cloudstorageapi.storage.provider.StorageUploadProvider;
@@ -10,7 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+
+import static org.example.cloudstorageapi.constant.StorageErrorMessages.Upload.DAMAGED_FILE;
 
 @Service
 @RequiredArgsConstructor
@@ -20,27 +25,26 @@ public class MinIOStorageUploadService<I> implements StorageUploadService<RespRe
     private final StorageResourceService<RespResourceInfoDTO, I> resourceService;
 
     @Override
-    public List<RespResourceInfoDTO> upload(I id, String path, List<MultipartFile> files) throws IOException {
+    public List<RespResourceInfoDTO> upload(I id, String path, List<MultipartFile> files){
 
         List<RespResourceInfoDTO> result = new ArrayList<>();
 
-        List<MultipartFile> sortedFiles = files.stream()
-                .sorted()
-                .toList();
+        try{
+            for (MultipartFile file : files){
+                provider.upload(id,
+                        path,
+                        file.getOriginalFilename(),
+                        file.getSize(),
+                        file.getContentType(),
+                        file.getInputStream()
+                );
 
-        for (MultipartFile file : sortedFiles){
-            provider.upload(id,
-                    path,
-                    file.getOriginalFilename(),
-                    file.getSize(),
-                    file.getContentType(),
-                    file.getInputStream()
-            );
-
-            RespResourceInfoDTO objectInfo = resourceService.getObjectInfo(id, path + file.getOriginalFilename());
-            result.add(objectInfo);
+                RespResourceInfoDTO objectInfo = resourceService.getObjectInfo(id, path + file.getOriginalFilename());
+                result.add(objectInfo);
+            }
+        }catch (IOException ex){
+            throw new StorageUploadException(DAMAGED_FILE, ex);
         }
-
         return result;
     }
 }
