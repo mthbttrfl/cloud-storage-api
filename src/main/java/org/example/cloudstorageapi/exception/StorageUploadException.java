05a7f998420cf7f16d260b9a6b27e05be59b385d
mthbttrfl <mthbttrfl@google.com>
2026-03-13 16:14:37 +0300

    feat: added method with parameters in the form of an error

--- src/main/java/org/example/cloudstorageapi/exception/StorageUploadException.java
+++ src/main/java/org/example/cloudstorageapi/exception/StorageUploadException.java
@@ -4,4 +4,8 @@ public class StorageUploadException extends StorageException {
     public StorageUploadException(String message) {
         super(message);
     }
+
+    public StorageUploadException(String message, Throwable throwable) {
+        super(message);
+    }
 }
