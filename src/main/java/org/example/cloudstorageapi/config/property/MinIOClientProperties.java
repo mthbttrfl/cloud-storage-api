cf8de2ae8b5ee545f4bce5d4198f00c1ecba1f7d
mthbttrfl <mthbttrfl@google.com>
2026-03-13 16:05:50 +0300

    refactor: properties
    remove @Component

--- src/main/java/org/example/cloudstorageapi/config/property/MinIOClientProperties.java
+++ src/main/java/org/example/cloudstorageapi/config/property/MinIOClientProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "minio.client")
-@Component
 @Getter
 @Setter
 public class MinIOClientProperties {
--- src/main/java/org/example/cloudstorageapi/config/property/UrlEndpointsProperties.java
+++ src/main/java/org/example/cloudstorageapi/config/property/UrlEndpointsProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "url.endpoints")
-@Component
 @Getter
 @Setter
 public class UrlEndpointsProperties {
